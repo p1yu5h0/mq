a2d3824f7925450fc586b06149359f4bd5178055
p1yu5h0 <piyush200205@gmail.com>
2024-01-17 06:43:45 +0530

    working queues

---
+++ 2_workqueues/new_task.js
@@ -0,0 +1,22 @@
+var amqp = require("amqplib/callback_api");
+
+amqp.connect("amqp://localhost", function (err0, connection) {
+  if (err0) throw err0;
+  connection.createChannel(function (err1, channel) {
+    if (err1) throw err1;
+    var queue = "task_queue";
+    var msg = process.argv.slice(2).join(" ") || "Hello World!";
+
+    channel.assertQueue(queue, {
+      durable: true,
+    });
+    channel.sendToQueue(queue, Buffer.from(msg), {
+      persistent: true,
+    });
+    console.log(" [x] Sent '%s'", msg);
+  });
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
